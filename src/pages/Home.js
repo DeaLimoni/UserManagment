dd9f1843e9a43719587e550bd1a95977572b787c
Dea Limoni <dea.limoni@student.uni-pr.edu>
2025-10-01 17:55:29 +0200

    home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import UserCard from "../components/UserCard";
+import SearchBar from "../components/SearchBar";
+import AddUserForm from "../components/AddUserForm";
+
+function Home() {
+  const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
