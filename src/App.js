4614705c6c47fd15fc5a7b2f1ccb4fc59cdcda61
Dea Limoni <dea.limoni@student.uni-pr.edu>
2025-10-01 17:46:10 +0200

    Change App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import UserDetailsPage from "./pages/UserDetailsPage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/user/:id" element={<UserDetailsPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
